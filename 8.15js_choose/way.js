063a304a72125fab252b6ecbd3a84fea1dc4d134
Ricky Tan <tan@RickydeMacBook-Pro.local>
2015-08-17 19:22:31 +0800

    js_choose

--- 8.15js_choose/way.js
+++ 8.15js_choose/way.js
@@ -16,23 +16,72 @@ all.addEventListener('click',function (event) {
 	}
 })
 
-inall.addEventListener('click',function (event) {
-	if (event.target.checked == false) {
-		for (var i = 0; i < one.length; i++) {
-			if (one[i].checked == false) {
-				one[i].checked = true;
-				i = i + 1;
-			};
+// inall.addEventListener('click',function (event) {
+// 	if (event.target.checked == false) {
+// 		for (var i = 0; i < one.length; i++) {
+// 			if (one[i].checked == false) {
+// 				one[i].checked = true;
+// 				i = i + 1;
+// 			};
+// 			one[i].checked = false;
+// 		};
+// 	};
+// 	if (event.target.checked == true) {
+// 		for (var i = 0; i < one.length; i++) {
+// 			if (one[i].checked == true) {
+// 				one[i].checked = false;
+// 				i = i + 1;
+// 			};
+// 			one[i].checked = true;
+// 		};
+// 	}
+// })
+
+inall.addEventListener('click',function(event){
+	for (var i = 0; i < one.length; i++) {
+		if(one[i].checked == false){
+			one[i].checked = true;
+		}
+		else{
 			one[i].checked = false;
 		};
 	};
-	if (event.target.checked == true) {
-		for (var i = 0; i < one.length; i++) {
-			if (one[i].checked == true) {
-				one[i].checked = false;
-				i = i + 1;
+	if (one[0].checked) {
+		if (one[1].checked) {
+			if (one[2].checked) {
+				if (one[3].checked) {
+					if (one[4].checked) {
+						all.checked = true;
+					};
+				};
 			};
-			one[i].checked = true;
 		};
 	}
+	else{
+		all.checked = false;
+	};
 })
+
+
+
+
+for (var i = 0; i < one.length; i++) {
+	one[i].addEventListener('click',function(event){
+		if (event.target.checked) {
+			if (one[0].checked) {
+				if (one[1].checked) {
+					if (one[2].checked) {
+						if (one[3].checked) {
+							if (one[4].checked) {
+								all.checked = true;
+							};
+						};
+					};
+				};
+			};
+		}
+		else{
+			all.checked = false;
+		}
+	})
+};
